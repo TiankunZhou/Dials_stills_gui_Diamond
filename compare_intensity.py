e78b3ad569ab6dfa753ff978a110c18d9370462f
Tiankun Zhou <Tiankun.zhou@diamond.ac.uk>
2023-11-15 17:01:09 +0000

    Add compare two intensity and heat map plot

--- compare_intensity.py
+++ compare_intensity.py
@@ -8,7 +8,6 @@ import argparse
 import glob
 import csv
 from pandas import *
-import multiprocessing
 
 
 # input file path and parameters
@@ -21,109 +20,59 @@ def process_args():
         help="give the input files as list, give only csv files"
         )
 
-    #cpu used
-    input_args.add_argument("-cpu","--cpu_use",
-        type=int,
-        help="set number of cpu used, default is 8",
-        default=8
-        )
-
     #save args
     args = input_args.parse_args()
+
+    if len(args.input_file) == 2:
+        pass
+    else:
+        print(f"Please give TWO csv files")
+        exit()
 
     return args
 
 
-def save_output(args, image_intensity:dict):
-    #get values
-    output_file = os.path.join(args.output_dir, args.output_prefix + ".csv")
-    image_intensity_list = list(map(list, image_intensity.items()))
-    image_intensity_list = sorted(image_intensity_list)
-    image_number = [i[0] for i in image_intensity_list]
-    all_intensity = [i[1][0] for i in image_intensity_list]
-    filtered_intensity = [i[1][1] for i in image_intensity_list]
-    diff_intensity = [i[1][2] for i in image_intensity_list]
-    sum_intensity_filtered_chip_remove = [i[1][3] for i in image_intensity_list]
-    image_moving_average = [i[1][4] for i in image_intensity_list]
+def plot_intensity(args):
+    file_name_0 = Path(args.input_file[0]).stem
+    data = read_csv(args.input_file[0], sep = "\t")
+    image_number_0= data["Image_Number"].tolist()
+    sum_intensity_0 = data["Filtered_Intensity_Chip_scattering_removed"].tolist()
 
-    #save csv
-    with open(output_file,"w", newline='') as output_csv:
-        write_csv = csv.writer(output_csv, dialect="excel-tab")
-        write_csv.writerow(["Image_Number", "Total_Intensity", "Filtered_intensity", "Intensity_Difference", "Filtered_Intensity_Chip_scattering_removed", "moving_average"])
-        for i in range(len(image_number)):
-            write_csv.writerow([image_number[i], all_intensity[i], filtered_intensity[i], diff_intensity[i], sum_intensity_filtered_chip_remove[i], image_moving_average[i]])
-    output_csv.close()
+    file_name_1 = Path(args.input_file[1]).stem
+    data = read_csv(args.input_file[1], sep = "\t")
+    image_number_1= data["Image_Number"].tolist()
+    sum_intensity_1 = data["Filtered_Intensity_Chip_scattering_removed"].tolist()
 
-    print(f"Spectra written to {output_file}")
-
-def plot_intensity(args, image_intensity:dict):
-    if args.data_format == "csv":
-        data = read_csv(args.input_file[0], sep = "\t")
-        image_number = data["Image_Number"].tolist()
-        sum_intensity_filtered_chip_remove = data["Filtered_Intensity_Chip_scattering_removed"].tolist()
-        diff_intensity = data["Intensity_Difference"].tolist()
-        all_intensity = data["Total_Intensity"].tolist()
-        filtered_intensity = data["Filtered_intensity"].tolist()
-        moving_averages = data["moving_average"].tolist()
-        #print(image_number, sum_intensity_filtered_chip_remove)
-        #exit()
-    else:
-        #print(image_intensity)
-        image_intensity_list = list(map(list, image_intensity.items()))
-        image_intensity_list = sorted(image_intensity_list)
-        for i in image_intensity_list:
-            print(i)
-        image_number = [i[0] for i in image_intensity_list]
-        all_intensity = [i[1][0] for i in image_intensity_list]
-        filtered_intensity = [i[1][1] for i in image_intensity_list]
-        diff_intensity = [i[1][2] for i in image_intensity_list]
-        sum_intensity_filtered_chip_remove = [i[1][3] for i in image_intensity_list]
-        image_moving_average = [i[1][4] for i in image_intensity_list]
-
-    """if max(sum_intensity_filtered_chip_remove) == 0:
-        norm_intensity = sum_intensity_filtered_chip_remove
-    else:
-        norm_intensity = [i/max(sum_intensity_filtered_chip_remove) for i in sum_intensity_filtered_chip_remove]"""
-
-    #print(image_number, sum_intensity_filtered_chip_remove, norm_intensity)
+    difference = [i - j for i,j in zip(sum_intensity_0, sum_intensity_1)]
+    difference_x = [i +1 for i in range(len(difference))]
 
     font = 12
     #plot all intensities
-    plt.figure("sum of intensities per image", figsize = (18, 10))
+    plt.figure(f"compare intensities: {file_name_0} and {file_name_1}", figsize = (18, 10))
     plt.subplot(211)
-    plt.plot(image_number, sum_intensity_filtered_chip_remove, marker="o")
+    plt.plot(image_number_0, sum_intensity_0, marker="o", label=f"{file_name_0}")
+    plt.plot(image_number_1, sum_intensity_1, marker="o", label=f"{file_name_1}")
     plt.ylabel("All intensities",fontsize=font,fontweight="bold")
     plt.xlabel("image number",fontsize=font)
+    plt.legend()
     plt.title(f"All intensities",fontsize=16)
-      
-    #plot intensity with moving average (intensity/max_intensity)
-    plt.subplot(212)
-    plt.plot(image_number, sum_intensity_filtered_chip_remove, marker="o", label="All intensity")
-    plt.plot(image_number, image_moving_average, marker="o", label="Moving average")
-    plt.ylabel("All intensities and moving average",fontsize=font,fontweight="bold")
-    plt.xlabel("image number",fontsize=font)
-    plt.title(f"All intensities with moving average",fontsize=16)
 
-    plt.subplots_adjust(left=0.075, bottom=0.05, right=0.95, top=0.95, hspace=0.5)
+    #plot intensity difference
+    plt.subplot(212)
+    plt.plot(difference_x, difference, marker="o", label=f"{file_name_0} - {file_name_1}")
+    plt.ylabel("All intensities",fontsize=font,fontweight="bold")
+    plt.xlabel("image number",fontsize=font)
+    plt.legend()
+    plt.title(f"Intensity difference",fontsize=16)
     plt.show()
     
 
 def main():
     args = process_args()
-    print(f"plot the sum of background intensities per image for Diamond cbf and other h5 files")
-
-    #get the image:intensity dictionary
-    starttime = time.time()
-    image_intens = read_and_sum_intensities(args)
-    #print(image_intens)
-    print('That took {} seconds'.format(time.time() - starttime))
-
-    #save the file
-    if args.save_output == True:
-        save_output(args, image_intens)
+    print(f"compareing intensities between:  {args.input_file[0]} and {args.input_file[1]}")
 
     #plot data
-    plot_intensity(args, image_intens)
+    plot_intensity(args)
 
 if __name__ ==  "__main__" :
     main()
--- plot_chip_heatmap.py
+++ plot_chip_heatmap.py
@@ -3,6 +3,7 @@ import os
 import sys
 from pathlib import Path
 from processing_stills import colors
+import seaborn as sns
 from matplotlib import pyplot as plt
 import argparse
 import glob
@@ -26,61 +27,185 @@ def process_args():
     args = input_args.parse_args()
     return args
 
-#convert the 1d list to 2d (n x n):
-def convert_1dto2d(data:list, row_number:int):
+#convert the 1d list to 2d (n x n), choose how to do the reverse: odd, even, eight, none
+def convert_1dto2d(data:list, row_number:int, reverse_option:str):
     new_list = []
     i = 0
     data_length = len(data)
-    print(f"Input 1d list length is {data_length}")
+    #print(f"Input 1d list length is {data_length}")
     while i + row_number < data_length:
-        print(i)
+        #print(i)
         list_section = [i for i in data[i:i+row_number]]
         new_list.append(list_section)
         i += row_number
     else:
-        print(f"last block: {i}")
+        #print(f"last block: {i}")
         list_section =[i for i in data[i:]]
         new_list.append(list_section)
+
+    #define reverse 
+    if reverse_option == "odd":
+        for i in range(len(new_list)):
+            if (i+1)%2 != 0:
+                new_list[i].reverse()
+    elif reverse_option == "even":
+        for i in range(len(new_list)):
+            if (i+1)%2 == 0:
+                new_list[i].reverse()
+    elif reverse_option == "eight":
+        for i in range(len(new_list)):
+            j = int((i/8) % 8)
+            if (j+1)%2 == 0:
+                new_list[i].reverse()
+    elif reverse_option == "none":
+        pass
     
     return new_list
 
-
 #convert the 1-D intensity data to the chip layout
 def convert_to_chip(data:list):
+    city_block_notconv = []
     converted_chip_data = []
-    block_list = convert_1dto2d(data, 400)
-    #print(block_list)
+    #create list with separate block intensities as 1d list: [[0...399], [400...799], [800...1199]...]
+    block_list = convert_1dto2d(data, 400, "eight")
 
+    #convert each 1d siti block to 20 x 20 1d lists, each 1d list has 20 values: [[0...19], [20...39], [40...59]...]
+    #then reverse the odd row (n+1, as starts with 0): [[19...0], [20...39], [59...40]...]
     for i, j in zip(block_list, range(len(block_list))):
         print(f"length of the sublist {j} is : {len(i)}")
-        well_list = convert_1dto2d(i, 20)
+        well_list = convert_1dto2d(i, 20, "odd")
         converted_chip_data.append(well_list)
+        #converted_chip_data.append(well_list)
     
     #print(converted_chip_data[0], converted_chip_data[-1])
     #print(len(converted_chip_data[0][0]), len(converted_chip_data[-1][-1]))
     return converted_chip_data
 
 
-def plot_heatmap(args):
+def check_yes_no(data:list):
+    yes_or_no = []
+    even_list = []
+    odd_list = []
+    
+    """create even list"""
+    for i in range(len(data)):
+        if (i+1)%2 == 0:
+            even_list.append(1)
+        else:
+            even_list.append(0)
+    
+        """create odd list"""
+        if (i+1)%2 != 0:
+            odd_list.append(1)
+        else:
+            odd_list.append(0)
+
+        """create yes_or_no list"""    
+        if i+1 < len(data) - 1 and i != 0:
+            if data[i] > data[i+1] and data[i] > data [i-1]:
+                yes_or_no.append(1)
+            elif data[i] < data[i+1] and data[i] < data [i-1]:
+                yes_or_no.append(0)
+            else:
+                if len(yes_or_no) == 1:
+                    yes_or_no.append(0)
+                else:
+                    if yes_or_no[-1] > yes_or_no[-2]:
+                        yes_or_no.append(0.49)
+                    elif yes_or_no[-1] < yes_or_no[-2]:
+                        yes_or_no.append(0.51)
+                    else:
+                        yes_or_no.append(0)
+        else:
+            yes_or_no.append(0) 
+    
+    """compare even and odd"""
+    even_compare = [1 if i - j == 0 else 0.5 if -0.9 < i - j < 0.9 else 0 for i, j in zip(even_list, yes_or_no)]
+    odd_compare = [1 if i - j == 0 else 0.5 if -0.9 < i - j < 0.9 else 0 for i, j in zip(odd_list, yes_or_no)]
+
+    #print(even_list)
+    #print(odd_list) 
+    return yes_or_no, even_compare, odd_compare
+
+
+def plot_heatmaps(args):
+    """get the list of intensities as 1d list"""
     file_name = Path(args.input_file).stem
     data = read_csv(args.input_file, sep = "\t")
     sum_intensity = data["Filtered_Intensity_Chip_scattering_removed"].tolist()
-
-    data = convert_to_chip(sum_intensity)
-    test = data[0]
-    test2 = data[1]
-
-    font = 12
-    #plot all intensities
-    plt.figure(f"Heatplot for chip: {file_name}", figsize = (18, 10))
     
-    plt.subplot(211)
-    plt.imshow(test, cmap='hot', interpolation='nearest')
+    """test chip layout"""
+    test_1d = []
+    for i in range(25600):
+        test_1d.append(i+1)
 
-    plt.subplot(212)
-    plt.imshow(test2, cmap='hot', interpolation='nearest')
+    """convert the 1d list intensities to the chip layout"""
+    data = convert_to_chip(sum_intensity)
+    yes_no, even, odd = check_yes_no(sum_intensity)
+    yes_no_chip = convert_to_chip(yes_no)
+    even_chip = convert_to_chip(even)
+    odd_chip = convert_to_chip(odd)
+    test = convert_to_chip(test_1d)
+    #print(yes_no)
+    #plot all intensities
+    #plt.figure(f"Heatplot for chip: {file_name}", figsize = (18, 10))
 
-    plt.show()
+    """asign blocksfor ploting"""
+    block_assign = []
+    for section in range(len(data)):
+        i = (section) % 8
+        j = int((section/8) % 8)
+
+        """reverse the city block in the even column (j+1 as column starts with 0)"""
+        if (j+1)%2 == 0 :
+            i = 7 - i
+        else:
+            i = i
+        block_assign.append([section, i, j])
+    
+    for test_block in block_assign:
+        print(f"block number: {test_block[0]},row: {test_block[1]}, column: {test_block[2]}")
+        print(yes_no_chip[test_block[0]])
+
+    """function for multi processing"""
+    def assign_chip(block_corr:list, datalist:list):
+        fig, axes = plt.subplots(nrows=8, ncols=8, sharex=True, sharey=True, figsize = (18, 10))
+        cbar_position = fig.add_axes([.85, .3, .05, .4])
+        for block in block_assign:
+            print(f"preparing: block {block[0]}, row {block[1]}, column {block[2]}")
+            if block[0] == 0:
+                sns.heatmap(datalist[block[0]], cmap="RdYlGn", annot=False, cbar=True, cbar_ax=cbar_position, linewidths=0.01, linecolor='white', ax=axes[block[1],block[2]])
+            else:
+                sns.heatmap(datalist[block[0]], cmap="RdYlGn", annot=False, cbar=False, linewidths=0.01, linecolor='white', ax=axes[block[1],block[2]])
+        fig.subplots_adjust(bottom=0.1, top=0.9, left=0.1, right=0.8, wspace=0.02, hspace=0.02)
+
+    """ploting"""
+    """Plot chip layout test"""
+    print(f"Preparing test")
+    #assign_chip(block_assign, test)
+    #plt.suptitle(f"Chip layout test for: {file_name}")
+
+    """plot overall intensity heatmap """
+    print(f"Preparing all intensity")
+    assign_chip(block_assign, data)
+    plt.suptitle(f"Intensity heatplot for chip: {file_name}")
+
+    """plot drop hit heatmap """
+    print(f"Preparing hit map")
+    assign_chip(block_assign, yes_no_chip)
+    plt.suptitle(f"Hits heatplot for chip: {file_name}")
+
+    """plot the odd match"""
+    print(f"Preparing odd match")
+    assign_chip(block_assign, odd_chip)
+    plt.suptitle(f"Odd match for chip: {file_name}")
+
+    """plot the even match"""
+    print(f"Preparing even match; Last one")
+    assign_chip(block_assign, even_chip)
+    plt.suptitle(f"Even match for chip: {file_name}")
+
+    #plt.show()
     
 
 def main():
@@ -88,7 +213,8 @@ def main():
     print(f"ploting chip heat map for : {args.input_file}")
 
     #plot data
-    plot_heatmap(args)
+    plot_heatmaps(args)
+    plt.show()
 
 if __name__ ==  "__main__" :
     main()
